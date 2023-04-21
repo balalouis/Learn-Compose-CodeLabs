9b2b2eaf21011900af0112dadac213df54c9dedb
SBALAMU1 <balan.ssn@gmail.com>
2023-04-21 15:38:51 +0530

    OnClick, recompose

--- app/src/main/java/com/learn/compose/MainActivity.kt
+++ app/src/main/java/com/learn/compose/MainActivity.kt
@@ -3,13 +3,10 @@ package com.learn.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,23 +25,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
-    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
-        Greeting(name = "Android")
+fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")
+){
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names){
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(text = "Hello !")
-            Text(text = name)
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        val expanded = remember { mutableStateOf(false) }
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+                Text(text = "Hello !")
+                Text(text = name)
+            }
+
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show Less" else "Show More")
+            }
         }
+
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     LearnComposeCodeLabsTheme {
