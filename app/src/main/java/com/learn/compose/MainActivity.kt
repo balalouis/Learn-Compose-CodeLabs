30cf93a19a4aaa67166001b5e58707a2b89abb70
SBALAMU1 <balan.ssn@gmail.com>
2023-04-21 12:56:34 +0530

    Re using composable

--- app/src/main/java/com/learn/compose/MainActivity.kt
+++ app/src/main/java/com/learn/compose/MainActivity.kt
@@ -3,13 +3,16 @@ package com.learn.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learn.compose.ui.theme.LearnComposeCodeLabsTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +21,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnComposeCodeLabsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
+fun MyApp(modifier: Modifier = Modifier){
+    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
+        Greeting(name = "Android")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(text = "Hello !")
+            Text(text = name)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LearnComposeCodeLabsTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
