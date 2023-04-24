9c5cda5aa108ba565465c1b62d4467893c2d293d
SBALAMU1 <balan.ssn@gmail.com>
2023-04-24 16:55:11 +0530

    Code refactor

--- app/src/main/java/com/learn/compose/MainActivity.kt
+++ app/src/main/java/com/learn/compose/MainActivity.kt
@@ -3,15 +3,20 @@ package com.learn.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,8 +42,11 @@ fun OnBoardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Uni
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Welcome to the Basics Codelab!")
-        ElevatedButton(onClick = onContinueClicked, Modifier.padding(12.dp)) {
-            Text(text = "Continue", color = MaterialTheme.colorScheme.secondary)
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
         }
     }
 }
@@ -46,10 +54,12 @@ fun OnBoardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Uni
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
     var shouldShowOnBoarding by remember  { mutableStateOf(true) }
-    if(shouldShowOnBoarding){
-        OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false})
-    }else{
-        Greetings()
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
+        if (shouldShowOnBoarding) {
+            OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
+        } else {
+            Greetings()
+        }
     }
 }
 
@@ -57,36 +67,54 @@ fun MyApp(modifier: Modifier = Modifier) {
 fun Greetings(modifier: Modifier = Modifier,names: List<String> = List(1000) { "$it" }){
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
-            Greeting(name = name)
+            CardContent(name = name)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
+fun CardContent(name: String) {
         val expanded = remember { mutableStateOf(false) }
-        val extraPadding by animateDpAsState(targetValue = if (expanded.value) 48.dp else 0.dp)
 
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)) {
-                Text(text = "Hello !", style = MaterialTheme.typography.bodyMedium)
-                Text(text = name, style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.ExtraBold))
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = 12.dp)
+            ) {
+                Text(text = "Hello !")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold)
+                )
+                if(expanded.value){
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
             }
 
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show Less" else "Show More")
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(
+                    imageVector = if (expanded.value) Filled.ExpandLess else Filled.ExpandMore,
+                    contentDescription = if (expanded.value) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
         }
-
-    }
 }
 
 @Preview(showBackground = true, widthDp = 320)
